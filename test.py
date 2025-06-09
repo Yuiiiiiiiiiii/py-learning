1a9fd16f0a6ef1175b8c9098af6b3302ff1ca74a
Tang Yujie <yui_333@qq.com>
2025-06-09 16:37:21 +0800

    kl

--- test.py
+++ test.py
@@ -1,20 +1,19 @@
-# 变量类型
-name = "Alice"  # str
-age = 20        # int
-grades = [90, 85, 88]  # list
-info = {"name": "Alice", "age": 20}  # dict
+def shuchu(tif_file):
+    # 打开TIFF文件
+    with raster
+        # 读取所有波段（假设波段顺序为B02, B03, B04, B08, B12）
+        bands = src.read()  # 形状为 (波段数, 高度, 宽度)，这里是 (5, height, width)
+        # profile = src.profile  # 获取元数据
 
-# 类型转换
-age_str = str(age)
-number = int("123")
+    # 分配波段（假设TIFF中的波段顺序为B02, B03, B04, B08, B12）
+    blue = bands[0].astype(float)   # B02 - 蓝
+    green = bands[1].astype(float)  # B03 - 绿
+    red = bands[2].astype(float)    # B04 - 红
+    nir = bands[3].astype(float)    # B08 - 近红外
+    swir = bands[4].astype(float)   # B12 - 短波红外
 
-# 作用域
-x = 10  # 全局变量
-def my_function():
-    y = 5  # 局部变量
-    global x
-    x += 1
-    print(f"Inside function: x={x}, y={y}")
-
-my_function()
-print(f"Outside function: x={x}")
+    # 真彩色正则化
+    rgb_orign = np.dstack((red, green, blue))
+    array_min, array_max = rgb_orign.min(), rgb_orign.max()
+    rgb_normalized = ((rgb_orign - array_min) / (array_max - array_min)) * 255
+    rgb_normalized = rgb_normalized.astype(np.uint8)
